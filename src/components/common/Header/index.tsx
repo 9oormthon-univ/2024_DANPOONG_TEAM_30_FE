be09dde011908a5eff7f649a09abb912dae90529
Ahnnakyung <nakyung.ahn.03@gmail.com>
2024-11-19 01:50:19 +0900

    홈 화면 퍼블리싱

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="url flex flex-row mt-[40px]">
+      <img
+        src="https://ifh.cc/g/jc33wA.png"
+        alt="Example"
+        className="w-[28px] h-[25px]"
+      />
+      <img
+        src="https://ifh.cc/g/BXvdas.png"
+        alt="Example"
+        className="w-[28px] h-[25px] ml-[255px]"
+      />
+      <button>
+        <img
+          src="https://ifh.cc/g/vGdgnr.png"
+          alt="Example"
+          className="w-[28px] h-[25px] ml-[10px]"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Header;
