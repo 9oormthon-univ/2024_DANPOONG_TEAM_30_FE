cfee25baa6cda9907356fecc5d96bb6d9f1bb2fd
leeleeleeleejun <wnstjr6293@naver.com>
2024-11-20 14:09:58 +0900

    rename: SelfCheckPage.tsx내용을 SelfCheckSurveyPage로 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,8 +4,7 @@ import HomePage from '@/pages/HomePage.tsx';
 import SelfCheckPage from '@/pages/SelfCheckPage.tsx';
 import NoneContainerLayout from '@/components/common/Layout/NoneContainerLayout.tsx';
 import SelfCheckResultPage from '@/pages/SelfCheckResultPage.tsx';
-import ProgramDetailPage from "@/pages/ProgramDetail/ProgramDetailPage";
-
+import ProgramDetailPage from '@/pages/ProgramDetail/ProgramDetailPage';
 
 const Router = () => {
   return (
@@ -14,12 +13,12 @@ const Router = () => {
         {/* Layout을 포함한 공통 구조 */}
         <Route element={<Layout />}>
           {/* HomePage 경로 */}
-          <Route path="/" element={<HomePage />} />
+          <Route path='/' element={<HomePage />} />
           {/* ProgramDetailPage 경로 */}
-          <Route path="/program/:id" element={<ProgramDetailPage />} />
+          <Route path='/program/:id' element={<ProgramDetailPage />} />
+          <Route path='/self-check' element={<SelfCheckPage />} />
         </Route>
         <Route element={<NoneContainerLayout />}>
-          <Route path='/self-check' element={<SelfCheckPage />} />
           <Route path='/self-check-result' element={<SelfCheckResultPage />} />
         </Route>
       </Routes>
---
+++ src/pages/SelfCheckSurveyPage.tsx
@@ -0,0 +1,34 @@
+import Question from '@/components/SelfCheck/Question.tsx';
+import Theme from '@/components/SelfCheck/Theme.tsx';
+import Gauge from '@/components/SelfCheck/Gauge.tsx';
+import SimpleHeader from '@/components/common/Header/SimpleHeader.tsx';
+import Container from '@/components/common/Layout/Container.tsx';
+
+const SelfCheckSurveyPage = () => {
+  return (
+    <>
+      <SimpleHeader title={'자립준비도 자가진단'} />
+      <Container>
+        <Gauge progress={10} />
+        <Theme
+          title={'건강에 관한 문제 입니다.'}
+          explain={'해당 질문에 맞는 항목에 체크해주세요.'}
+        />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <div className={'my-[20px] flex gap-[16px]'}>
+          <button className={`${button} bg-gray2 text-gray50`}>이전</button>
+          <button className={`${button} bg-gray2 text-gray50`}>다음</button>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default SelfCheckSurveyPage;
+
+const button = 'w-[50%] h-[50px] rounded-[10px]';
