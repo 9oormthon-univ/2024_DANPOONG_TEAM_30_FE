9e11c87529294ed0418a47bc4eb1e2fc3714c1eb
leeleeleeleejun <wnstjr6293@naver.com>
2024-11-21 20:59:41 +0900

    design: 온보딩 질문 페이지 디자인 수정

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,34 +1,34 @@
-import SimpleHeader from "@/components/common/Header/SimpleHeader";
-import Container from "@/components/common/Layout/Container";
-import OIcon from "@/assets/icons/O.svg?react";
-import XIcon from "@/assets/icons/X.svg?react";
+import SimpleHeader from '@/components/common/Header/SimpleHeader';
+import Container from '@/components/common/Layout/Container';
+import OIcon from '@/assets/icons/O.svg?react';
+import XIcon from '@/assets/icons/X.svg?react';
 
 const QuestionPage = () => {
   return (
-    <div>
-      <SimpleHeader title={""} />
+    <>
+      <SimpleHeader title={''} />
       <Container>
-        <div className="mt-[32px] font-bold text-2xl">
+        <div className='mt-[32px] font-bold text-2xl'>
           어쩌고저쩌고 질문입닏아아ㅏ
         </div>
-        <div className="font-semibold text-lg">
+        <div className='font-semibold text-lg'>
           관심 기반으로 정책을 소개해드려요
         </div>
-        <div className="flex flex-row justify-center items-center mt-[231px] gap-8">
-          <div className="w-[126px] h-[126px] bg-emerald-50 rounded-lg text-emerald-400 text-md p-10 border-2 border-white hover:border-emerald-400">
-            <OIcon className="mb-[4px]" />
+        <div className='flex flex-row justify-center items-center my-auto gap-8'>
+          <div className='w-[126px] h-[126px] flex flex-col justify-center items-center bg-emerald-50 rounded-lg text-emerald-400 text-md border-2 border-white hover:border-emerald-400'>
+            <OIcon className='mb-[4px]' />
             그렇다
           </div>
-          <div className="w-[126px] h-[126px] bg-red2 rounded-lg text-red text-md p-10 text-center border-2 border-white hover:border-rose-400">
-            <XIcon className="mb-[6px]" />
+          <div className='w-[126px] h-[126px] flex flex-col justify-center items-center bg-red2 rounded-lg text-red text-md border-2 border-white hover:border-rose-400'>
+            <XIcon className='mb-[4px]' />
             아니다
           </div>
         </div>
-        <button className="w-full h-[55px] rounded-xl p-[16px] mt-[185px] bg-zinc-300 text-zinc-500 hover:bg-main hover:text-black">
+        <button className='w-full h-[55px] rounded-xl p-[16px] mb-[40px] bg-zinc-300 text-zinc-500 hover:bg-main hover:text-black'>
           다음
         </button>
       </Container>
-    </div>
+    </>
   );
 };
 
