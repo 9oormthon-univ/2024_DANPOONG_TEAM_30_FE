af9c3de35be6229e6916f5a514cf9bda81541a83
Ahnnakyung <nakyung.ahn.03@gmail.com>
2024-11-23 22:18:13 +0900

    홈 화면 뱃지 확인하기 자가진단 연결

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,74 +1,99 @@
-import ImageIcon from '@/assets/icons/Image.svg?react';
-import VectorIcon from '@/assets/icons/Vector.svg?react';
-import BannerIcon from '@/assets/icons/Banner.svg?react';
-import ProgramItem from '@/components/common/ProgramItem/ProgramItem';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ImageIcon from "@/assets/icons/Image.svg?react";
+import VectorIcon from "@/assets/icons/Vector.svg?react";
+import ProgramItem from "@/components/common/ProgramItem/ProgramItem";
+import CharacterIcon from "@/assets/icons/Character.svg?react";
+import CharacterIcon2 from "@/assets/icons/Character2.svg?react";
 
 const HomePage: React.FC = () => {
   const a = [0, 0, 0];
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  // 뱃지 확인하기 버튼 클릭 시 이동하는 함수
+  const handleBadgeClick = () => {
+    setIsBadgeChecked((prev) => !prev); // 상태 토글
+    navigate("/self-check"); // '/self-check-result'로 이동
+  };
+
+  const [isBadgeChecked, setIsBadgeChecked] = useState(false); // 버튼 클릭 상태 관리
+
   return (
     <div className="w-full h-screen overflow-hidden">
       <div className="h-full overflow-y-auto no-scrollbar">
-        <div className="">
-          <div className="h-[183px] rounded-xl relative">
-            <BannerIcon className="object-cover rounded-xl w-full" />
-            <button className="absolute bottom-5 right-4 bg-orange-400 text-black px-4 py-2 rounded-lg shadow-lg hover:bg-orange-500">
+        <div>
+          <div
+            className="w-full h-[183px] bg-cover bg-center rounded-xl relative"
+            style={{
+              backgroundImage: "url('https://ifh.cc/g/zkGrK4.png')",
+            }}
+          >
+            {isBadgeChecked ? (
+              <CharacterIcon2 className="absolute bottom-4 left-5" />
+            ) : (
+              <CharacterIcon className="absolute bottom-4 left-5" />
+            )}
+            <button
+              className="absolute bottom-5 right-6 bg-main text-black px-4 py-2 rounded-lg shadow-lg "
+              onClick={handleBadgeClick} // 버튼 클릭 핸들러 연결
+            >
               뱃지 확인하기
             </button>
           </div>
-          <div className='text-font2xLarge mt-[34px] ml-[4px] text-xl font-semibold'>
+          <div className="text-font2xLarge mt-[34px] ml-[4px] text-xl font-semibold">
             아임레디님을 위한 추천 프로그램
           </div>
-          <div className={'flex flex-col gap-[16px] mt-[16px]'}>
+          <div className={"flex flex-col gap-[16px] mt-[16px]"}>
             {a.map((_, i) => (
               <ProgramItem key={i} />
             ))}
           </div>
           <img
-            src='https://ifh.cc/g/dYrWaA.png'
-            alt='Example'
-            className='custom-img mt-[35px]'
+            src="https://ifh.cc/g/dYrWaA.png"
+            alt="Example"
+            className="custom-img mt-[35px]"
           />
-          <div className='text-xl mt-[34px] mb-[34px] ml-[4px] font-semibold'>
+          <div className="text-xl mt-[34px] mb-[34px] ml-[4px] font-semibold">
             꼭 필요한 자립 지식
           </div>
-          <div className='bg-zinc-100 h-[305px] rounded-xl mb-[10px] py-[6px] px-[20px] flex flex-col'>
-            <div className='mt-[20px] flex flex-row justify-between items-center'>
-              <div className='flex'>
+          <div className="bg-zinc-100 h-[305px] rounded-xl mb-[10px] py-[6px] px-[20px] flex flex-col">
+            <div className="mt-[20px] flex flex-row justify-between items-center">
+              <div className="flex">
                 <ImageIcon />
-                <div className='flex flex-col ml-[20px] font-medium'>
-                  <div className='mt-[6px] text-sm'>
+                <div className="flex flex-col ml-[20px] font-medium">
+                  <div className="mt-[6px] text-sm">
                     10월부터 달라지는 정부 지원 제도
                   </div>
-                  <div className='text-sm'>알아보기</div>
+                  <div className="text-sm">알아보기</div>
                 </div>
               </div>
-              <VectorIcon className='ml-[60px]' />
+              <VectorIcon className="ml-[60px]" />
             </div>
-            <div className='mt-[20px] flex flex-row justify-between items-center'>
-              <div className='flex'>
+            <div className="mt-[20px] flex flex-row justify-between items-center">
+              <div className="flex">
                 <ImageIcon />
-                <div className='flex flex-col ml-[20px] font-medium'>
-                  <div className='mt-[6px] text-sm'>
+                <div className="flex flex-col ml-[20px] font-medium">
+                  <div className="mt-[6px] text-sm">
                     10월부터 달라지는 정부 지원 제도
                   </div>
-                  <div className='text-sm'>알아보기</div>
+                  <div className="text-sm">알아보기</div>
                 </div>
               </div>
-              <VectorIcon className='ml-[60px]' />
+              <VectorIcon className="ml-[60px]" />
             </div>
-            <div className='mt-[20px] flex flex-row justify-between items-center'>
-              <div className='flex'>
+            <div className="mt-[20px] flex flex-row justify-between items-center">
+              <div className="flex">
                 <ImageIcon />
-                <div className='flex flex-col ml-[20px] font-medium'>
-                  <div className='mt-[6px] text-sm'>
+                <div className="flex flex-col ml-[20px] font-medium">
+                  <div className="mt-[6px] text-sm">
                     10월부터 달라지는 정부 지원 제도
                   </div>
-                  <div className='text-sm'>알아보기</div>
+                  <div className="text-sm">알아보기</div>
                 </div>
               </div>
-              <VectorIcon className='ml-[60px]' />
+              <VectorIcon className="ml-[60px]" />
             </div>
-            <button className='w-full mx-auto h-[50px] bg-orange-400 mt-[20px] rounded-xl'>
+            <button className="w-full mx-auto h-[50px] bg-orange-400 mt-[20px] rounded-xl">
               더 알아보기
             </button>
           </div>
